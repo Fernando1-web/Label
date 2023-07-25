98f26b64bb423169ebbbd0f35da480f42d0bf33b
28904 <28904@KNOBBLEBOY>
2023-07-24 23:19:52 -0600

    Agregar archivos de proyecto.

---
+++ Label/Label/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Label
+{
+    /// <summary>
+    /// <createddate>2023/7/2</createddate>
+    /// <company>INDEL</company>
+    /// <lastmodificationdate>2023/7/2</lastmodificationdate>
+    /// <lastmodificationdescription>se crearon etiquetas para una aplicacion movil</lastmodificationdescription>
+    /// <lastmodifierautor>Fernando Jose Majano Aguirre</lastmodifierautor>
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// Método constructor que se inicia al abrir la app
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
